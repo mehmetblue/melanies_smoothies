3c3d110103a745cc0f287a9f6e3c1524daf6673d
Mehmet Koç <98655470+mehmetblue@users.noreply.github.com>
2025-02-19 16:18:53 +0100

    Update streamlit_app.py
    nutrition requests blok moved in the `for fruit_chosen` block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -32,6 +33,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     # st.write(ingredients_string)
 
@@ -50,8 +53,6 @@ if ingredients_list:
         # st.success('Your Smoothie is ordered, '+ name_on_order,'!', icon="✅")
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-# New section to display smoothiefruit nutrition information
-import requests
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+
