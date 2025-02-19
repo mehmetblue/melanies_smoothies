92175b9be0c6978aa33c352a25596f45e87db1ef
Mehmet Koç <98655470+mehmetblue@users.noreply.github.com>
2025-02-19 16:01:39 +0100

    Update streamlit_app.py
    `requests` library and API block added

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         # st.success('Your Smoothie is ordered, '+ name_on_order,'!', icon="✅")
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+# New section to display smoothiefruit nutrition information
+import requests
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
